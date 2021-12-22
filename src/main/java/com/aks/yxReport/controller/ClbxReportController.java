0a8a7b0c34ff49216d1c9acd61b1f90218ee976f
Leagueofgent <gent1998@163.com>
2021-12-22 23:43:15 +0800

    Improve ClbxReportController.java

--- src/main/java/com/aks/yxReport/controller/ClbxReportController.java
+++ src/main/java/com/aks/yxReport/controller/ClbxReportController.java
@@ -4,7 +4,9 @@ import com.aks.yxReport.service.impl.ClbxReportServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @Controller
 public class ClbxReportController {
 
